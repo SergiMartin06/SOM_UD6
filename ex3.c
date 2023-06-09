932519a07db1a7aaf39061a3f2f439067d22d613
SergiMartin06 <smartin19238@iesjoanramis.org>
2023-06-09 21:48:39 +0200

    cambio local

--- ex3.c
+++ ex3.c
@@ -5,6 +5,6 @@ int main()
     int valor_variable;
     printf("Ingresa un valor");
     scanf("%d", &valor_variable);
-    printf("El valor de la variable es %s",&valor_variable);
+    printf("El valor de la variable es %d", valor_variable);
     return 0;
 }
